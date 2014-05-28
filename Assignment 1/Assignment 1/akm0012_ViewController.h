60c4cc30cfaed48bec86f582f910c58e75991411
akm0012 <akm0012@auburn.edu>
2014-05-27 20:26:19 -0500

    Initial Code Commit.

---
+++ Assignment 1/Assignment 1/akm0012_ViewController.h
@@ -0,0 +1,13 @@
+//
+//  akm0012_ViewController.h
+//  Assignment 1
+//
+//  Created by Andrew Marshall on 5/27/14.
+//  Copyright (c) 2014 Comp_7970_akm0012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface akm0012_ViewController : UIViewController
+
+@end
